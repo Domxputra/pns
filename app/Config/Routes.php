a136fd6bc16eae0e04a037d44c430793c8fe269d
Juan Philbert Artori Kris <juanplx283@gmail.com>
2023-10-11 09:28:51 +0900

    new

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/jurusan', 'jurusan::index');
+$routes->get('/jurusan/index', 'jurusan::index');
+
